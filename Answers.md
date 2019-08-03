53f9df31dc637bd4e031601343cb4ea140f7d2ba
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-08-03 10:45:42 -0700

    Created CharacterCard for Characters. Working on questions.

--- Answers.md
+++ Answers.md
@@ -2,10 +2,14 @@
 
 1. What is React JS and what problems does it try and solve? Support your answer with concepts introduced in class and from your personal research on the web.
 
-1. What does it mean to think in react?
+--React makes web apps faster by rendering the screen only when it's able to see the changes in state. React knows the change in state by using a setter function that we call once we call the setter function inside the useEffect hook.
 
-1. Describe state.
+2. What does it mean to think in react?
 
-1. Describe props.
+3. Describe state.
 
-1. What are side effects, and how do you sync effects in a React component to state or prop changes?
+4. Describe props.
+
+--Props (properties) is another word for a parameter that you are able to pass into a function so you can share between components.
+
+5. What are side effects, and how do you sync effects in a React component to state or prop changes?
--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CharacterCard from './components/CharacterCard.js';
 import './App.css';
 
 const App = () => {
@@ -9,9 +11,33 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState();
+
+  useEffect(() => {
+    axios
+    .get('https://swapi.co/api/people/')
+    .then(response => {
+      //console.log('Here is character info: ',response)
+      setCharacters(response.data.results)
+    })
+    .catch(error => console.log('There was an error: ', error))
+  }, [])
+
+  if (!characters) { return <div>loading</div> }
+  console.log('Character info: ', characters)
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <div className="CardRow">
+      {
+        Object.values(characters).map(character =>
+          <CharacterCard key={character.name} character={character} />
+        )
+      }
+
+      </div>
+     
     </div>
   );
 }
